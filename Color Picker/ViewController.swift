4292d55b9f4a99a8b6473c1d4e673c820f5404d5
Angelo Luppino <angelo.luppino@spcapitaliq.com>
2016-02-25 21:35:10 -0500

    Update to version 1

--- Color Picker/ViewController.swift
+++ Color Picker/ViewController.swift
@@ -7,19 +7,83 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var redValue: UITextField!
+    @IBOutlet weak var greenValue: UITextField!
+    @IBOutlet weak var blueValue: UITextField!
+    
+    @IBOutlet weak var hexValue: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        colorView.layer.borderWidth = 5;
+        colorView.layer.borderColor = UIColor.whiteColor().CGColor
+    }
+    
+    @IBAction func changeRedSlider(sender: UISlider!) {
+        redSlider.value = floor(redSlider.value)
+        redValue.text = String(Int(redSlider.value))
+        calculateColor(nil)
+    }
+    
+    @IBAction func changeGreenSlider(sender: UISlider!) {
+        greenSlider.value = floor(greenSlider.value)
+        greenValue.text = String(Int(greenSlider.value))
+        calculateColor(nil)
+    }
+    
+    @IBAction func changeBlueSlider(sender: UISlider!) {
+        blueSlider.value = floor(blueSlider.value)
+        blueValue.text = String(Int(blueSlider.value))
+        calculateColor(nil)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func calculateColor(sender: AnyObject!) {
+        let r = CGFloat(redSlider.value) / 255
+        let g = CGFloat(greenSlider.value) / 255
+        let b = CGFloat(blueSlider.value) / 255
+        
+        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+        
+        let rHex = String(format:"%02X", Int(255*r))
+        let gHex = String(format:"%02X", Int(255*g))
+        let bHex = String(format:"%02X", Int(255*b))
+        
+        hexValue.text = "#\(rHex)\(gHex)\(bHex)"
     }
 
+    @IBAction func enterRedValue(sender: UITextField) {
+        if (Float(redValue.text!) > 255) {
+            redValue.text = "255"
+        }
+        redSlider.value = Float(redValue.text!)!
+        calculateColor(nil)
+    }
+
+    @IBAction func enterGreenValue(sender: UITextField) {
+        if (Float(greenValue.text!) > 255) {
+            greenValue.text = "255"
+        }
+        greenSlider.value = Float(greenValue.text!)!
+        calculateColor(nil)
+    }
+    
+    @IBAction func enterBlueValue(sender: UITextField) {
+        if (Float(blueValue.text!) > 255) {
+            blueValue.text = "255"
+        }
+        blueSlider.value = Float(blueValue.text!)!
+        calculateColor(nil)
+    }
 
 }
 
